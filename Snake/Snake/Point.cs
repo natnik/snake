79954bdbd792ab1a8f3325dad996fc72d1858590
Natalya <nat.nik07@yahoo.com>
2016-07-09 14:04:18 +0300

    добавили 2 конструктора с параметрами и без

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -8,13 +8,24 @@ namespace Snake
 {
     class Point
     {
-       public int x;
-       public int y;
-       public char sym;
-       public void Draw()
-       {
-           Console.SetCursorPosition(x, y);
-           Console.Write(sym);
-       }
+        public int x;
+        public int y;
+        public char sym;
+
+        public Point()
+        { }
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
     }
 }
